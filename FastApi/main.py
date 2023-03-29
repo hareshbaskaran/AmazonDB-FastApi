cbfda1782b71e5a67f68cac1251931da8f5093e0
hareshbaskaran <baskaranharesh@gmail.com>
2023-03-29 20:23:33 +0530

    password_bearer Oauth2

---
+++ FastApi/main.py
@@ -0,0 +1,33 @@
+from typing import Annotated
+from schema import User,UserInDB,fake_hash_password,fake_users_db,get_current_active_user
+from fastapi import Depends, FastAPI,HTTPException
+from fastapi.security import OAuth2PasswordBearer,OAuth2PasswordRequestForm
+
+app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@app.get("/items/")
+async def read_items(token: Annotated[str, Depends(oauth2_scheme)]):
+    return {"token": token}
+
+
+@app.post("/token")
+async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
+    user_dict = fake_users_db.get(form_data.username)
+    if not user_dict:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    user = UserInDB(**user_dict)
+    hashed_password = fake_hash_password(form_data.password)
+    if not hashed_password == user.hashed_password:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user.username, "token_type": "bearer"}
+
+
+@app.get("/users/me")
+async def read_users_me(
+    current_user: Annotated[User, Depends(get_current_active_user)]
+):
+    return current_user
