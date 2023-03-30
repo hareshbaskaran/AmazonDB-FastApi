f5f9e43740b4d1663df228b0997dd1a9b7dbdc1b
hareshbaskaran <baskaranharesh@gmail.com>
2023-03-30 14:21:58 +0530

    database declaration

---
+++ FastApi/db.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+#import postgresql connection link below
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/db_name"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
